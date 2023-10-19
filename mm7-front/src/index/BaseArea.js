3f75c8dcb4c9d6976b2aecf1336dc94784a947bb
Eric Yu <yu.czeric@gmail.com>
2023-10-19 11:41:10 +0200

    add variable for height

--- mm7-front/src/index/BaseArea.js
+++ mm7-front/src/index/BaseArea.js
@@ -3,14 +3,14 @@ import React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 
-const BaseArea = ({ backgroundImage, children }) => {
+const BaseArea = ({ backgroundImage, vheight="60vh", children }) => {
   const areaStyle = {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
     minWidth: '100%',
-    height: '80vh',
+    height: vheight,
     backgroundColor: '#d8f3dc',
     backgroundSize: 'cover',
     backgroundPosition: 'center center',
