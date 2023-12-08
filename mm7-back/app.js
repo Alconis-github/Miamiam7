e0d2c5edce7b3f64b438ad56f656d1b538764377
Alconis-github <ncombal@gmail.com>
2023-12-08 16:48:12 +0100

    Fix lien back db

--- mm7-back/app.js
+++ mm7-back/app.js
@@ -13,7 +13,7 @@ const { PrismaClient } = require ('@prisma/client')
 const prisma = new PrismaClient({
     datasources: {
       db: {
-        url: 'bd_mm7.db'
+        url: 'file:./bd_mm7.db'
       }
     },
     log: ['info', 'query', 'warn', 'error']
@@ -22,15 +22,28 @@ const prisma = new PrismaClient({
 
 app.get('/', (req, res) => res.send('Hello, World!'))
 
+app.get('/api/recipes', async (req, res) => {
+  const result = await prisma.recipes.findMany()
+  res.send(result)
+})
+
+app.get('/api/recipes/:id', async (req, res) => {
+  const result = await prisma.recipes.findUnique({
+    where: {id: parseInt(req.params.id)}
+  })
+  res.send(result)
+})
+
+app.get('/api/recipe-of-the-day', async (req, res) => {
+  const result = await prisma.recipes.findFirst({
+    where: {ofTheDay: true}
+  })
+  res.send(result)
+})
+
 prisma.$connect
 
-app.get('/api/recipes', async (req, res) => {
-    const result = await prisma.recipes.findUnique({
-      where: {
-        id: 1,
-      },
-    })
-})
+
 
 //app.get('/api/recipes/:id', (req, res) => {
 //    const id = parseInt(req.params.id)
