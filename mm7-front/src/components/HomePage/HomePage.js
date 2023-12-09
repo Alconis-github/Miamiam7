e15e0a54f103d5791fb7139b5bf8cf7fecdf870a
Alconis-github <ncombal@gmail.com>
2023-12-09 19:06:52 +0100

    resolved conflicts

--- mm7-front/src/components/HomePage/HomePage.js
+++ mm7-front/src/components/HomePage/HomePage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
-import accueilImg from '../../assets/images/HomePage/accueil.jpg';
 import BaseArea from './BaseArea';
 import SearchSection from './SearchSection';
 import CategorySelection from './CategorySelection';
@@ -9,7 +8,17 @@ import entreeIcon from '../../assets/images/HomePage/entree.png';
 import platIcon from '../../assets/images/HomePage/plat.png';
 import dessertIcon from '../../assets/images/HomePage/dessert.png';
 import boissonIcon from '../../assets/images/HomePage/boisson.png';
-import Category from './CategorySelection';
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 function HomePage() {
 
@@ -24,8 +33,8 @@ function HomePage() {
   const innerContainerStyle = {
     minWidth: '25%',
     minHeight: '50%',
-    backgroundColor: 'rgba(255, 255, 255, 0.8)',
     textAlign: 'center',
+    padding: "50px 0",
   };
 
   const flexC_ContainerStyle = {
@@ -35,12 +44,21 @@ function HomePage() {
     alignItems: 'center',
     ...innerContainerStyle
   }
+
   const flexR_ContainerStyle = {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     ...innerContainerStyle
+  }
+
+  const hero = {
+    backgroundColor: '#B23E3E',
+    maxWidth: "80%",
+    padding: "10px",
+    borderRadius: '12px',
+    ...flexC_ContainerStyle
   }
 
   const categories = [
@@ -62,32 +80,61 @@ function HomePage() {
     <>
     {recipe&&(
     <>
-    <BaseArea backgroundImage={accueilImg} vheight='60vh'>
-      <Container style={flexC_ContainerStyle}>
-        <Typography variant="h1" color="text.primary" paragraph maxWidth="md">
+    <BaseArea vheight='60vh' bgColor="#53664C" p="60px">
+      <Container style={hero}>
+        <Typography variant="h2" color="white" paragraph maxWidth="md" style={{fontWeight: 'bold'}}>
           UN GRIMOIRE DE RECETTES 100% N7 ET 100% BDD
         </Typography>
-        <Typography variant="h5" color="text.secondary" paragraph maxWidth="md">
+        <Typography variant="h5" color="white" paragraph maxWidth="md">
           Retrouvez les recettes concoctées par nos meilleurs chefs cuisiniers de l'N7.
         </Typography>
       </Container>
     </BaseArea>
-    <SearchSection></SearchSection>
+
+    <BaseArea>
+        <Container style={flexC_ContainerStyle}>
+            <Typography variant="h2" color="text.primary" paragraph maxWidth="md" style={{fontWeight: 'bold'}}>
+                    LA VEDETTE DU JOUR
+            </Typography>
+          <Container style={flexR_ContainerStyle} backgroundColor="red">
+              <img 
+              src={require(`../../assets/images/Recipes/${recipe.image}`)}
+              alt={recipe.name}
+              style={{
+                  width: '250px',
+                  height: '250px',
+                  maxWidth:'80%',
+              }}/>
+              <Container style={flexC_ContainerStyle} >
+                <Typography variant="h3" color="text.primary" paragraph maxWidth="md" style={{fontWeight: 'bold'}}>
+                      {recipe.name}
+                </Typography>
+                <Typography variant="h5" color="text.primary" paragraph maxWidth="md">
+                        {recipe.description}
+                </Typography>
+              </Container>
+          </Container>
+        </Container>
+    </BaseArea>
+
+    <BaseArea>
+      <SearchSection></SearchSection>
+    </BaseArea>
 
     <BaseArea>
       <Container style={flexC_ContainerStyle}> 
-      <Typography variant="h3" color="text.primary" paragraph maxWidth="md">
+      <Typography variant="h3" color="text.primary" paragraph maxWidth="md" style={{fontWeight: 'bold'}} >
           RECHERCHEZ UN PLAT SPECIFIQUE
         </Typography>        
-        <Typography variant="h3" color="text.primary" paragraph maxWidth="md">
+        <Typography variant="h3" color="text.primary" paragraph maxWidth="md" style={{fontWeight: 'bold'}}>
           OU
         </Typography>
-        <Typography variant="h3" color="text.primary" paragraph maxWidth="md">
+        <Typography variant="h3" color="text.primary" paragraph maxWidth="md" style={{fontWeight: 'bold'}}>
           DECOUVREZ UNE CATEGORIE
         </Typography>
       </Container>
     </BaseArea>
-
+    
     <BaseArea>
         <Container>
           <div style={{ ...categoriesContainerStyle, flexWrap: 'wrap' }}>
@@ -98,26 +145,17 @@ function HomePage() {
         </Container>
       </BaseArea>
 
-
     <BaseArea>
-        <Container style={flexR_ContainerStyle}>
-        <Container style={flexC_ContainerStyle}>
-            <Typography variant="h2">
-                Recette Vedette { recipe.name }
-            </Typography>
-            <img 
-            src={require(`../../assets/images/Recipes/${recipe.image}`)}
-            alt={ recipe.image }
-            style={{
-                width: '250px',
-                height: '250px',
-            }}/>
-        
-        </Container>
-        <Container>
-            { recipe.description }
-        </Container>
-        </Container>
+      <Typography variant="h2" color="text.primary" paragraph maxWidth="md" style={{fontWeight: 'bold'}}>
+        RECETTES RECENTES
+      </Typography>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} padding="5% 30%">
+        {Array.from(Array(6)).map((_, index) => (
+          <Grid item xs={2} sm={4} md={4} key={index}>
+            <Item>RECETTE</Item>
+          </Grid>
+        ))}
+      </Grid>
     </BaseArea>
     </>
     )}
--- mm7-front/src/components/HomePage/SearchSection.js
+++ mm7-front/src/components/HomePage/SearchSection.js
@@ -1,37 +1,76 @@
-import { useState } from "react";
-import IconButton from "@mui/material/IconButton";
-import SearchIcon from "@mui/icons-material/Search";
-import TextField from "@mui/material/TextField";
-import BaseArea from "./BaseArea";
+import * as React from 'react';
+import { useAutocomplete } from '@mui/base/useAutocomplete';
+import { styled } from '@mui/system';
 
-function SearchSection() {
-    const [searchQuery, setSearchQuery] = useState("");
-    const SearchBar = ({setSearchQuery}) => (
-        <form >
-          <TextField
-            sx={{width: '500px',}}
-            id="search-bar"
-            onInput={(e) => {
-              setSearchQuery(e.target.value);
-            }}
-            label="Qu'allons nous manger aujourd'hui ?"
-            variant="outlined"
-            placeholder="Search..."
-            size="large"
-          />
-        <IconButton size="large" type="submit" aria-label="search" style={{ backgroundColor: 'red', borderRadius: '12px' }}>
-          <SearchIcon style={{ fill: "white" }} />
-        </IconButton>
-        </form>
-      );
+const Label = styled('label')({
+  display: 'block',
+});
 
-    return (
-        <BaseArea vheight="10vh">
-            <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
-        </BaseArea>
-    );
+const Input = styled('input')(({ theme }) => ({
+  width: 200,
+  backgroundColor: theme.palette.mode === 'light' ? '#fff' : '#000',
+  color: theme.palette.mode === 'light' ? '#000' : '#fff',
+}));
+
+const Listbox = styled('ul')(({ theme }) => ({
+  width: 200,
+  margin: 0,
+  padding: 0,
+  zIndex: 1,
+  position: 'absolute',
+  listStyle: 'none',
+  backgroundColor: theme.palette.mode === 'light' ? '#fff' : '#000',
+  overflow: 'auto',
+  maxHeight: 200,
+  border: '1px solid rgba(0,0,0,.25)',
+  '& li.Mui-focused': {
+    backgroundColor: '#4a8df6',
+    color: 'white',
+    cursor: 'pointer',
+  },
+  '& li:active': {
+    backgroundColor: '#2977f5',
+    color: 'white',
+  },
+}));
+
+export default function UseAutocomplete() {
+
+  const [recipes, setRecipes] = React.useState()
+  React.useEffect(() => {
+    fetch("http://localhost:3333/api/recipes")
+    .then((response) => response.json())
+    .then((data) => setRecipes(data))
+    .catch((err) => { console.log(err); });
+  },[])
+
+  const {
+    getRootProps,
+    getInputLabelProps,
+    getInputProps,
+    getListboxProps,
+    getOptionProps,
+    groupedOptions,
+  } = useAutocomplete({
+    id: 'use-autocomplete-demo',
+    options: recipes,
+    getOptionLabel: (option) => option.name,
+  });
+
+
+  return (
+    <div>
+      <div {...getRootProps()}>
+        <Label {...getInputLabelProps()}>Qu'allons nous manger aujourd'hui ?</Label>
+        <Input {...getInputProps()} />
+      </div>
+      {groupedOptions.length > 0 ? (
+        <Listbox {...getListboxProps()}>
+          {groupedOptions.map((option, index) => (
+            <li {...getOptionProps({ option, index })}>{option.name}</li>
+          ))}
+        </Listbox>
+      ) : null}
+    </div>
+  );
 }
-
-
-
-export default SearchSection
