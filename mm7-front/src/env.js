5ec23a2f4e63aadb386084b3a241eac4cc5dbbbc
Alconis-github <ncombal@gmail.com>
2023-12-10 18:51:46 +0100

    Added a note

--- mm7-front/src/env.js
+++ mm7-front/src/env.js
@@ -1,3 +1,5 @@
+//Pour passer en local, décommenter les lignes 4 et 6 et commenter les lignes 3 et 4
+
 const URL_BACK = "https://miamiam7.smash-house.fr:3333"
 //const URL_BACK = "http://localhost:3333"
 const URL_FRONT = "https://miamiam7.smash-house.fr"
